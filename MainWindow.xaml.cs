abd32c2775960ea3b4140d6bb7e9b5a2a6e18bb7
AbhishekSCCTech <abhishek.suryawanshi@cctech.co.in>
2024-12-04 16:40:59 +0530

    Add project file.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Printing;
+using System.Security.Cryptography.Xml;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -17,139 +19,160 @@ namespace Paint
     /// </summary>
     public partial class MainWindow : Window
     {
-        private bool isDrawing = false; 
-        private Point startPoint;       
-        private Brush selectedColor = Brushes.Black; 
-        private double selectedBrushSize = 2;        
-        private string drawingMode = "Free Draw";    
+        Line currentLine;
+        bool paintStart = false;
+        Point StartPoint;
+        string drawMode;
+        Brush selectedColor;
+        double strokeThickness;
 
         public MainWindow()
         {
             InitializeComponent();
-            BrushSize.SelectedIndex = 0;
-            BrushColor.SelectedIndex = 4;
-            Mode.SelectedIndex = 0;
-
-            BrushSize.SelectionChanged += BrushSize_SelectionChanged;
-            BrushColor.SelectionChanged += BrushColor_SelectionChanged;
-            Mode.SelectionChanged += Mode_SelectionChanged;
         }
+
         private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
+            paintStart = true;
+            StartPoint = e.GetPosition(this);
+            if (drawMode == "Straight Line")
             {
-                isDrawing = true;
-                startPoint = e.GetPosition(Canvas);
-
-                if (drawingMode == "Straight Line")
+                currentLine = new Line()
                 {
-                    // Start line drawing
-                    var line = new Line
-                    {
-                        Stroke = selectedColor,
-                        StrokeThickness = selectedBrushSize,
-                        X1 = startPoint.X,
-                        Y1 = startPoint.Y,
-                        X2 = startPoint.X,
-                        Y2 = startPoint.Y
-                    };
-
-                    Canvas.Children.Add(line);
-                    line.CaptureMouse();
-                }
+                    X1 = StartPoint.X,
+                    Y1 = StartPoint.Y,
+                    X2 = StartPoint.X,
+                    Y2 = StartPoint.Y,
+                    Stroke = selectedColor,  // Set the stroke color to black
+                    StrokeThickness = strokeThickness
+                };
+                Canvas.Children.Add(currentLine);
             }
         }
 
         private void Canvas_MouseMove(object sender, MouseEventArgs e)
         {
-            if (!isDrawing || e.LeftButton != MouseButtonState.Pressed)
-                return;
-
-            if (drawingMode == "Free Draw")
+            if (!paintStart) return;
+            Point currentPoint = e.GetPosition(Canvas);
+            if (drawMode == "Straight Line" && currentLine != null)
             {
-                // Draw freehand line
-                var currentPoint = e.GetPosition(Canvas);
-                var line = new Line
+                // Update the end point of the line
+                currentLine.X2 = currentPoint.X;
+                currentLine.Y2 = currentPoint.Y;
+            }
+            else if (drawMode == "Free Draw")
+            {
+                // Draw small lines for free drawing
+                var freeLine = new Line()
                 {
-                    Stroke = selectedColor,
-                    StrokeThickness = selectedBrushSize,
-                    X1 = startPoint.X,
-                    Y1 = startPoint.Y,
+                    X1 = StartPoint.X,
+                    Y1 = StartPoint.Y,
                     X2 = currentPoint.X,
-                    Y2 = currentPoint.Y
+                    Y2 = currentPoint.Y,
+                    Stroke = selectedColor,
+                    StrokeThickness = strokeThickness
                 };
+                Canvas.Children.Add(freeLine);
+                StartPoint = currentPoint; // Update starting point for the next segment
+            }
 
-                Canvas.Children.Add(line);
-                startPoint = currentPoint;
-            }
-            else if (drawingMode == "Straight Line")
-            {
-                if (Canvas.Children[Canvas.Children.Count - 1] is Line lastLine)
-                {
-                    var currentPoint = e.GetPosition(Canvas);
-                    lastLine.X2 = currentPoint.X;
-                    lastLine.Y2 = currentPoint.Y;
-                }
-            }
         }
 
         private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Released)
-            {
-                isDrawing = false;
-
-                if (drawingMode == "Straight Line")
-                {
-                    // Release mouse capture for line drawing
-                    if (Canvas.Children[Canvas.Children.Count - 1] is Line lastLine)
-                        lastLine.ReleaseMouseCapture();
-                }
-            }
+            paintStart = false;
+            currentLine = null; ;
         }
-
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Canvas.Children.Clear();
+
+        }
+        private void Mode_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Mode.SelectedItem is ComboBoxItem selectedItem)
+            {
+                if (selectedItem.Content is string selectedText)
+                {
+                    if (selectedText == "Straight Line")
+                    {
+                        drawMode = "Straight Line";
+                    }
+                    else if (selectedText == "Free Draw")
+                    {
+                        drawMode = "Free Draw";
+                    }
+                    else
+                    {
+                        drawMode = string.Empty; 
+                    }
+                }
+            }
 
         }
         private void BrushColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (BrushColor.SelectedItem is ComboBoxItem selectedItem)
             {
-                selectedColor = selectedItem.Content.ToString() switch
+                if (selectedItem.Content is string selectedText)
                 {
-                    "Red" => Brushes.Red,
-                    "Green" => Brushes.Green,
-                    "Yellow" => Brushes.Yellow,
-                    "Blue" => Brushes.Blue,
-                    "Black" => Brushes.Black,
-                    _ => Brushes.Black,
-                };
+                    if (selectedText == "Red")
+                    {
+                        selectedColor = Brushes.Red;
+                    }
+                    else if (selectedText == "Green")
+                    {
+                        selectedColor = Brushes.Green;
+                    }
+                    else if (selectedText == "Yellow")
+                    {
+                        selectedColor = Brushes.Yellow;
+                    }
+                    else if (selectedText == "Blue")
+                    {
+                        selectedColor = Brushes.Blue;
+                    }
+                    else if (selectedText == "Black")
+                    {
+                        selectedColor = Brushes.Black;
+                    }
+                    else
+                    {
+                        selectedColor = Brushes.Black; // Default to black
+                    }
+                }
             }
-
         }
-
-        private void BrushSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void StrokeThickness_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (BrushSize.SelectedItem is ComboBoxItem selectedItem && selectedItem.Content != null)
+            if (BrushSize.SelectedItem is ComboBoxItem selectedItem)
             {
-                selectedBrushSize = double.Parse(selectedItem.Content.ToString());
+                if (selectedItem.Content is string selectedText)
+                {
+                    // Set strokeThickness based on selected value directly
+                    switch (selectedText)
+                    {
+                        case "2":
+                            strokeThickness = 2;
+                            break;
+                        case "4":
+                            strokeThickness = 4;
+                            break;
+                        case "6":
+                            strokeThickness = 6;
+                            break;
+                        case "8":
+                            strokeThickness = 8;
+                            break;
+                        case "10":
+                            strokeThickness = 10;
+                            break;
+                        default:
+                            strokeThickness = 2; // Fallback to default if no match
+                            break;
+                    }
+                }
             }
         }
-
-        private void Mode_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (Mode.SelectedItem is ComboBoxItem selectedItem && selectedItem.Content != null)
-            {
-                drawingMode = selectedItem.Content.ToString();
-            }
-        }
-
-
-
-
-
     }
 }
